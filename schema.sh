de2c472f3fc0564d9f08a1cdc3fd812b58810fd9
Steevan BARBOYON <sephirothlune@free.fr>
2014-03-10 14:25:21 +0100

    Delete clear cache

--- schema.sh
+++ schema.sh
@@ -2,16 +2,6 @@
 
 source vendor/kujaff/sf2scripts/functions.sh
 
-#####################
-# vidage des caches #
-#####################
-
-execCmdNoEcho "./vendor/kujaff/sf2scripts/cache.sh $sf2env"
-
-###############################
-# mise à jour de la structure #
-###############################
-
 title "Update database"
 execConsole "doctrine:schema:update --force"
 

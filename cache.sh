26494849d4dcd3389a36dc8d501e2d10f86bb50a
Steevan BARBOYON <sephirothlune@free.fr>
2014-03-15 20:32:01 +0100

    Remove asstic from cache

--- cache.sh
+++ cache.sh
@@ -7,9 +7,5 @@ title "Delete caches"
 execCmd "sudo rm -rf app/cache/*"
 execCmd "sudo chmod -R 777 app/cache"
 
-if [ "$sf2env" = "prod" ]; then
-    execConsole "assetic:dump"
-fi
-
 echoOk
 exit 0

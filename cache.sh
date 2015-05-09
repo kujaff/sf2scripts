9363d4117e9ca370ace31a3c5e9e62f1cff5d528
Steevan BARBOYON <sephirothlune@free.fr>
2015-05-09 11:36:36 +0200

    Use setfacl

--- cache.sh
+++ cache.sh
@@ -5,7 +5,6 @@ source vendor/kujaff/sf2scripts/functions.sh
 title "Delete caches"
 
 execCmd "sudo rm -rf app/cache/*"
-execCmd "sudo chmod -R 777 app/cache"
+createDir777 "app/cache"
 
 echoOk
-exit 0
